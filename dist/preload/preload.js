a679b8837ea63feee1ed7877bc44b5165827e5c2
TopJamMan <me@jaspermansfield.me>
2025-10-28 23:18:39 +0000

    Initial commit

---
+++ dist/preload/preload.js
@@ -0,0 +1,116 @@
+"use strict";
+const electron = require("electron");
+function _0x5b393f(_0x3245db, _0x236381, _0x4070a4, _0x469984) {
+  return _0x2c46(_0x469984 - 830, _0x236381);
+}
+function _0x2c46(_0xbbfb75, _0x158027) {
+  var _0x211101 = _0x5497();
+  return _0x2c46 = function(_0x5bf5172, _0x327fc32) {
+    _0x5bf5172 = _0x5bf5172 - 102;
+    var _0x4044f8 = _0x211101[_0x5bf5172];
+    return _0x4044f8;
+  }, _0x2c46(_0xbbfb75, _0x158027);
+}
+(function(_0x2ba455, _0x5ded5d) {
+  function _0x2d0a24(_0x2512f5, _0x4d4426, _0x144a51, _0xf88e4) {
+    return _0x2c46(_0x4d4426 - 880, _0x2512f5);
+  }
+  var _0x1fde76 = _0x2ba455();
+  function _0x419a77(_0x3c82b8, _0x1f0889, _0x4c88a5, _0x3cf1e1) {
+    return _0x2c46(_0x1f0889 - -12, _0x3c82b8);
+  }
+  while (!![]) {
+    try {
+      var _0x70b66d = parseInt(_0x419a77(146, 125, 94, 156)) / 1 * (parseInt(_0x2d0a24(1026, 1041, 1033, 1011)) / 2) + parseInt(_0x2d0a24(1049, 1032, 1045, 1043)) / 3 * (-parseInt(_0x2d0a24(982, 991, 1020, 964)) / 4) + -parseInt(_0x2d0a24(994, 1015, 993, 1034)) / 5 + parseInt(_0x2d0a24(993, 1001, 1020, 991)) / 6 + -parseInt(_0x2d0a24(1008, 1008, 985, 987)) / 7 + -parseInt(_0x419a77(106, 112, 96, 100)) / 8 * (parseInt(_0x2d0a24(1045, 1038, 1062, 1068)) / 9) + parseInt(_0x2d0a24(1035, 1020, 1038, 1021)) / 10 * (parseInt(_0x2d0a24(982, 982, 1008, 966)) / 11);
+      if (_0x70b66d === _0x5ded5d) break;
+      else _0x1fde76["push"](_0x1fde76["shift"]());
+    } catch (_0xb37526) {
+      _0x1fde76["push"](_0x1fde76["shift"]());
+    }
+  }
+})(_0x5497, 640607);
+var _0x327fc3 = /* @__PURE__ */ (function() {
+  var _0x39f602 = !![];
+  return function(_0x359161, _0x2b83cd) {
+    var _0x5843fc = _0x39f602 ? function() {
+      function _0xde3269(_0x70f376, _0x11f74b, _0x435427, _0x3df6d6) {
+        return _0x2c46(_0x435427 - 423, _0x3df6d6);
+      }
+      if (_0x2b83cd) {
+        var _0x23f373 = _0x2b83cd[_0xde3269(560, 577, 576, 594)](_0x359161, arguments);
+        return _0x2b83cd = null, _0x23f373;
+      }
+    } : function() {
+    };
+    return _0x39f602 = ![], _0x5843fc;
+  };
+})();
+function _0x473178(_0x4161ed, _0x3b6564, _0x1cfb60, _0x5db7bb) {
+  return _0x2c46(_0x1cfb60 - 225, _0x4161ed);
+}
+function _0x5497() {
+  var _0x4c464a = ["return (function() ", "9CSqBtA", "apply", "discord-update-activity", "get-license-key_OBS", "electron", "length", "7542OQDjDL", "qXTgK", "electronAPI", "2HJPJjP", "constructor", "set-license-key_OBS", "132eXTdsZ", "license-validation-failed_OBS", "update-downloaded", "telemetry-track", '{}.constructor("return this")( )', "get-language", "exposeInMainWorld", "get-save-files-from-directory", "trace", "599692hgRUWk", "update-available", "set-language", "invoke", "__proto__", "find-route-path-order", "telemetry-initialize", "tDFCj", "app-quit", "build-blueprints", "7151220IXwUNf", "telemetry-flush", "CgEeH", "10464uMmkUy", "get-data-server-port", "log", "get-version", "989296YycNzl", "removeListener", "table", "load-data-file", "update-progress", "console", "error", "232685OTzxQX", "info", "1110247akDrsz", "save-game-auto", "remove-license-key_OBS", "60190DMXTfH", "load-game-from-file", "warn", "bind", "load-game-from-path", "exception", "prototype", "save-game-to-file", "toString", "delete-save-file", "removeAllListeners"];
+  _0x5497 = function() {
+    return _0x4c464a;
+  };
+  return _0x5497();
+}
+var _0x5bf517 = _0x327fc3(void 0, function() {
+  var _0x13d83b = function() {
+    function _0x4c6b0b(_0x52c622, _0x32b686, _0x552e6a, _0x576dd8) {
+      return _0x2c46(_0x52c622 - 366, _0x32b686);
+    }
+    var _0x3507d5;
+    function _0x42da72(_0xd8ab5e, _0x1902b7, _0x9be0e4, _0x28c40f) {
+      return _0x2c46(_0x9be0e4 - -990, _0x1902b7);
+    }
+    try {
+      if (_0x42da72(-888, -881, -867, -862) === _0x4c6b0b(489, 493, 483, 478)) _0x3507d5 = Function(_0x42da72(-850, -856, -839, -855) + _0x42da72(-884, -894, -884, -897) + ");")();
+      else return _0x1984a7["on"](_0x4c6b0b(478, 469, 509, 486), (_0x167b40, _0x4a1a1c) => _0x5a2b7c(_0x4a1a1c)), () => _0xddfaa9[_0x4c6b0b(516, 530, 508, 512)](_0x4c6b0b(478, 470, 476, 505));
+    } catch (_0x7e25b2) {
+      _0x42da72(-818, -858, -831) === _0x42da72(-852, -883, -872) ? _0xf95866 = _0x81ea9 : _0x3507d5 = window;
+    }
+    return _0x3507d5;
+  }, _0x2ea13c = _0x13d83b(), _0x372688 = _0x2ea13c[_0x4d8513(564, 594, 589, 588)] = _0x2ea13c[_0x4d8513(564, 593, 581, 555)] || {};
+  function _0x4d3d03(_0x59c0fd, _0x29ddcb, _0x490738, _0x346251) {
+    return _0x2c46(_0x346251 - -733, _0x59c0fd);
+  }
+  function _0x4d8513(_0x18c525, _0x1e15e0, _0x35a72c, _0xf8e897) {
+    return _0x2c46(_0x18c525 - 431, _0xf8e897);
+  }
+  var _0x32c75c = [_0x4d3d03(-585, -624, -617, -607), _0x4d8513(573, 550, 589, 575), _0x4d8513(567, 540, 556, 547), _0x4d8513(565, 590, 588, 568), _0x4d8513(576, 600, 558, 574), _0x4d8513(561, 537, 568, 584), _0x4d3d03(-626, -653, -649, -623)];
+  for (var _0x553dba = 0; _0x553dba < _0x32c75c[_0x4d3d03(-601, -558, -599, -576)]; _0x553dba++) {
+    var _0x4a5bb4 = _0x327fc3[_0x4d8513(593, 569, 612, 585)][_0x4d8513(577, 569, 557, 567)][_0x4d3d03(-592, -562, -611, -590)](_0x327fc3), _0x1d0b9d = _0x32c75c[_0x553dba], _0xe74990 = _0x372688[_0x1d0b9d] || _0x4a5bb4;
+    _0x4a5bb4[_0x4d8513(546, 518, 553, 555)] = _0x327fc3[_0x4d3d03(-562, -562, -567, -590)](_0x327fc3), _0x4a5bb4[_0x4d8513(579, 554, 609, 560)] = _0xe74990[_0x4d3d03(-566, -577, -584, -585)][_0x4d3d03(-607, -611, -579, -590)](_0xe74990), _0x372688[_0x1d0b9d] = _0x4a5bb4;
+  }
+});
+_0x5bf517();
+electron.contextBridge[_0x5b393f(948, 962, 946, 938)](_0x473178(395, 415, 385), { "loadDataFile": (_0xe548d3) => electron.ipcRenderer[_0x473178(357, 330, 339)](_0x473178(387, 356, 356), _0xe548d3), "getDataServerPort": () => electron.ipcRenderer[_0x5b393f(964, 975, 947, 944)](_0x5b393f(981, 968, 967, 955)), "buildBlueprints": (_0xd63e1) => electron.ipcRenderer[_0x5b393f(938, 929, 929, 944)](_0x5b393f(924, 922, 956, 950), _0xd63e1), "findRoutePathOrder": (_0x427052) => electron.ipcRenderer[_0x5b393f(964, 969, 950, 944)](_0x5b393f(920, 952, 939, 946), _0x427052) }), electron.contextBridge[_0x5b393f(942, 962, 910, 938)](_0x5b393f(955, 968, 1008, 986), { "quit": () => electron.ipcRenderer[_0x5b393f(917, 960, 956, 944)](_0x5b393f(970, 968, 961, 949)), "updateDiscordActivity": (_0x30394c, _0x2949a2) => electron.ipcRenderer[_0x5b393f(951, 971, 936, 944)](_0x5b393f(968, 1011, 958, 984), _0x30394c, _0x2949a2), "saveGameToFile": (_0x5ba24d) => electron.ipcRenderer[_0x473178(320, 326, 339)](_0x5b393f(968, 982, 983, 977), _0x5ba24d), "saveGameAuto": (_0x1747e3) => electron.ipcRenderer[_0x5b393f(958, 962, 974, 944)](_0x473178(383, 358, 363), _0x1747e3), "loadGameFromFile": () => electron.ipcRenderer[_0x473178(360, 367, 339)](_0x473178(338, 335, 366)), "loadGameFromPath": (_0x386bd9) => electron.ipcRenderer[_0x473178(353, 351, 339)](_0x473178(341, 378, 369), _0x386bd9), "getSaveFilesFromDirectory": () => electron.ipcRenderer[_0x5b393f(957, 967, 933, 944)](_0x5b393f(963, 912, 917, 939)), "deleteSaveFile": (_0x112af5) => electron.ipcRenderer[_0x5b393f(965, 947, 959, 944)](_0x5b393f(1003, 949, 965, 979), _0x112af5), "setLicenseKey": (_0x411508) => electron.ipcRenderer[_0x5b393f(914, 927, 964, 944)](_0x5b393f(965, 985, 974, 993), _0x411508), "getLicenseKey": () => electron.ipcRenderer[_0x5b393f(922, 947, 943, 944)](_0x5b393f(993, 983, 975, 985)), "removeLicenseKey": () => electron.ipcRenderer[_0x5b393f(943, 961, 955, 944)](_0x473178(381, 335, 364)), "getVersion": () => electron.ipcRenderer[_0x473178(358, 360, 339)](_0x473178(335, 352, 352)), "onLValidationFailed": (_0x4a8664) => {
+  function _0x1732f9(_0x53b33a, _0x1d5073, _0x40a827, _0x40881b) {
+    return _0x5b393f(_0x53b33a - 270, _0x40881b, _0x40a827 - 22, _0x1d5073 - -1649);
+  }
+  return electron.ipcRenderer["on"](_0x1732f9(-736, -716, -721, -696), (_0x921ef2, _0x2ffdb9) => _0x4a8664(_0x2ffdb9)), () => electron.ipcRenderer[_0x1732f9(-691, -669, -638, -662)](_0x1732f9(-724, -716, -687, -687));
+}, "onUpdateAvailable": (_0x12893b) => {
+  electron.ipcRenderer["on"](_0x5a175b(882, 875, 892), (_0x1fdc8d, _0x34022c) => _0x12893b(_0x34022c));
+  function _0x1af46c(_0x48b360, _0x356e5d, _0x50aca4, _0x1d977e) {
+    return _0x5b393f(_0x48b360 - 14, _0x356e5d, _0x50aca4 - 324, _0x50aca4 - -1762);
+  }
+  function _0x5a175b(_0x1a6edb, _0x21f364, _0x5593a9, _0x493a70) {
+    return _0x5b393f(_0x1a6edb - 34, _0x21f364, _0x5593a9 - 450, _0x1a6edb - -60);
+  }
+  return () => electron.ipcRenderer[_0x5a175b(920, 941, 922)](_0x1af46c(-819, -799, -820));
+}, "onUpdateProgress": (_0x9aebcf) => {
+  function _0xb1028d(_0x22547c, _0x401c5d, _0x105773, _0x31c8e4) {
+    return _0x473178(_0x22547c, _0x401c5d - 315, _0x105773 - -971);
+  }
+  return electron.ipcRenderer["on"](_0xb1028d(-622, -605, -614), (_0x58fd9e, _0x478dbf) => _0x9aebcf(_0x478dbf)), () => electron.ipcRenderer[_0xb1028d(-570, -574, -596)](_0xb1028d(-608, -633, -614));
+}, "onUpdateDownloaded": (_0x33ee6f) => {
+  electron.ipcRenderer["on"](_0x4e1ffd(-571, -573, -576), (_0xecc955, _0x5b875e) => _0x33ee6f(_0x5b875e));
+  function _0x57cb26(_0x5d6ad5, _0x465c41, _0x1239b1, _0x2b0ab9) {
+    return _0x5b393f(_0x5d6ad5 - 63, _0x1239b1, _0x1239b1 - 61, _0x465c41 - -1654);
+  }
+  function _0x4e1ffd(_0x111c1a, _0xd12f24, _0x252af1, _0x2582f5) {
+    return _0x5b393f(_0x111c1a - 141, _0xd12f24, _0x252af1 - 92, _0x111c1a - -1505);
+  }
+  return () => electron.ipcRenderer[_0x57cb26(-643, -674, -697)](_0x57cb26(-725, -720, -703));
+}, "telemetryInitialize": () => electron.ipcRenderer[_0x5b393f(917, 971, 962, 944)](_0x473178(325, 343, 342)), "telemetryTrack": (_0x212305) => electron.ipcRenderer[_0x473178(322, 333, 339)](_0x473178(332, 343, 330), _0x212305), "telemetryFlush": () => electron.ipcRenderer[_0x473178(312, 329, 339)](_0x5b393f(945, 982, 928, 952)), "getLanguage": () => electron.ipcRenderer[_0x5b393f(948, 966, 939, 944)](_0x473178(354, 321, 332)), "setLanguage": (_0xe568b7) => electron.ipcRenderer[_0x473178(356, 329, 339)](_0x473178(346, 343, 338), _0xe568b7), "ipcRenderer": { "on": (_0x5845d2, _0x505cba) => electron.ipcRenderer["on"](_0x5845d2, _0x505cba), "removeListener": (_0x5194c3, _0x4060e5) => electron.ipcRenderer[_0x5b393f(989, 962, 974, 959)](_0x5194c3, _0x4060e5) } });
